7edd9d28e4dbe222c8658d6f468fc6840400aeeb
Louis-Saglio <louis.saglio@sfr.fr>
2017-05-17 09:26:36 +0200

    Maj

---
+++ Cartes_pour_terminal/buffer.py
@@ -0,0 +1,5 @@
+def get_map(self):
+    return self.map
+
+def set_map(self, new_map):
+    self.map = new_map
---
+++ builtins/ascii.py
@@ -0,0 +1,11 @@
+a = "azertyuiopéù$£^¨µ§/.?.,:#{[|"
+# print(ascii(a))
+
+b = bin(256)
+print(b)
+
+
+def d(c):
+    return (c * c - c) / (c + c)
+
+
