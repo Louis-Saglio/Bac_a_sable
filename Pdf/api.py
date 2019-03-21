527ef6e582a5bc75e9a5184e7f31d2876c1201eb
Louis-Saglio <louis.saglio@sfr.fr>
2019-03-21 16:02:45 +0100

    Add possibility to have emplacements on all document pages

--- Pdf/api.py
+++ Pdf/api.py
@@ -22,22 +22,25 @@ class PdfTemplate:
         else:
             raise PdfException("There is no emplacement named {} in this PDF template".format(key))
 
-    def _fill_emplacements(self, canvas):
+    def _fill_emplacements(self, canvas, page_num):
         for name, value in self.values.items():
+            if value is None:
+                continue
             emplacement_data = self.emplacements[name]
             line_nbr = len(emplacement_data)
             for line_num in range(line_nbr):
-                line_size = emplacement_data[line_num][2]
-                if value is not None:
-                    chunk = value[line_size * line_num : (line_size * line_num + line_size)]
-                    if chunk:
-                        canvas.drawString(*emplacement_data[line_num][:2], chunk)
+                if emplacement_data[line_num][0] != page_num:
+                    continue
+                line_size = emplacement_data[line_num][3]
+                chunk = value[line_size * line_num: (line_size * line_num + line_size)]
+                if chunk:
+                    canvas.drawString(*emplacement_data[line_num][1:3], chunk)
         canvas.save()
 
-    def _build_watermark_pdf_filled_with_values(self):
+    def _build_watermark_pdf_filled_with_values_for_page(self, page_num):
         packet = io.BytesIO()
         canvas = reportlab.pdfgen.canvas.Canvas(packet, reportlab.lib.pagesizes.letter)
-        self._fill_emplacements(canvas)
+        self._fill_emplacements(canvas, page_num)
         packet.seek(0)
         return PyPDF2.PdfFileReader(packet)
 
@@ -47,12 +50,14 @@ class PdfTemplate:
         with open(self.base_file_path, "rb") as f:
             reader = PyPDF2.PdfFileReader(f)
 
-            page0 = reader.getPage(0)
-            page0.mergePage(self._build_watermark_pdf_filled_with_values().getPage(0))
-
-            output.addPage(page0)
-            output.addPage(reader.getPage(1))
-            output.addPage(reader.getPage(2))
+            for page_num in range(reader.getNumPages()):
+                page = reader.getPage(page_num)
+                watermark = self._build_watermark_pdf_filled_with_values_for_page(page_num)
+                if watermark.getNumPages() > 0:
+                    # A strange behaviour of PyPDF makes PdfFileReader built with a blank canvas contains no page at all
+                    # It is useful to check if there is anything we need to add to the actual pdf page
+                    page.mergePage(watermark.getPage(0))
+                output.addPage(page)
 
             with open(self.output_file_path, "wb") as final:
                 output.write(final)
--- Pdf/new.py
+++ Pdf/new.py
@@ -2,43 +2,45 @@ import Pdf.api as api
 
 
 template = api.PdfTemplate(
-    "contrat.pdf",
-    "out.pdf",
-    {
-        "tenant_full_name": ((175, 715, 27),),
-        "owner_full_name": ((375, 715, 27),),
-        "tenant_birthday": ((175, 700, 27),),
-        "owner_birthday": ((375, 700, 27),),
-        "tenant_phone": ((175, 685, 27),),
-        "owner_phone": ((375, 685, 27),),
-        "tenant_address": ((175, 670, 27), (175, 656.5, 27)),
-        "owner_address": ((374, 670, 26), (374, 656.5, 26)),
-        "tenant_postal_code": ((175, 640, 27),),
-        "owner_postal_code": ((375, 640, 27),),
-        "tenant_city": ((175, 625, 27),),
-        "owner_city": ((375, 625, 27),),
-        "storage_type": ((175, 564, 60),),
-        "storage_size": ((175, 549, 60),),
-        "storage_address": ((175, 535, 60), (175, 519.6, 60), (175, 504, 60)),
+    base_file_path="contrat.pdf",
+    output_file_path="out.pdf",
+    emplacements={
+        # emplacement_name": ((page_num, x, y, max_size), (same but for line 2), (same but for line x))
+        "tenant_full_name": ((0, 175, 715, 27),),
+        "owner_full_name": ((0, 375, 715, 27),),
+        "tenant_birthday": ((0, 175, 700, 27),),
+        "owner_birthday": ((0, 375, 700, 27),),
+        "tenant_phone": ((0, 175, 685, 27),),
+        "owner_phone": ((0, 375, 685, 27),),
+        "tenant_address": ((0, 175, 670, 27), (0, 175, 656.5, 27)),
+        "owner_address": ((0, 374, 670, 26), (0, 374, 656.5, 26)),
+        "tenant_postal_code": ((0, 175, 640, 27),),
+        "owner_postal_code": ((0, 375, 640, 27),),
+        "tenant_city": ((0, 175, 625, 27),),
+        "owner_city": ((0, 375, 625, 27),),
+        "storage_type": ((0, 175, 564, 60),),
+        "storage_size": ((0, 175, 549, 60),),
+        "storage_address": ((0, 175, 535, 5), (1, 175, 519.6, 6), (2, 175, 504, 60)),
         "access_conditions": ((175, 491, 60),),
-        "start_date": ((175, 430, 60),),
-        "end_date": ((175, 414, 60),),
-        "booked_surface": ((175, 399, 60),),
-        "owner_share": ((175, 378, 60),),
-        "undersigned_tenant": ((108, 329, 25),),
-        "has_photocopy": ((49, 208.5, 1),),
-        "has_proof of residence": ((49, 193, 1),),
-        "surety bond": ((49, 179, 1),),
-        "signature date_day": ((49, 81, 2),),
-        "signature date_month": ((73, 81, 2),),
-        "signature date_year": ((100, 81, 4),),
+        "start_date": ((0, 175, 430, 60),),
+        "end_date": ((0, 175, 414, 60),),
+        "booked_surface": ((0, 175, 399, 60),),
+        "owner_share": ((0, 175, 378, 60),),
+        "undersigned_tenant": ((0, 108, 329, 25),),
+        "has_photocopy": ((0, 49, 208.5, 1),),
+        "has_proof of residence": ((0, 49, 193, 1),),
+        "surety bond": ((0, 49, 179, 1),),
+        "signature date_day": ((0, 49, 81, 2),),
+        "signature date_month": ((0, 73, 81, 2),),
+        "signature date_year": ((0, 100, 81, 4),),
     },
 )
 
 template["tenant_full_name"] = "Louis Saglio"
-template["owner_full_name"] = "Laure Courty"
+template["owner_full_name"] = "Jean Dupont"
 template["tenant_birthday"] = "07/09/1998"
 template["tenant_address"] = "lorem ipsum dolor sit amet donec adipiscing consequetur etiam"
 template["owner_address"] = "smerjognf  ojsfdngvo osdij fôsijfdôsrigjôijsfog ijpij insdfunv usdf"
+template["storage_address"] = "smerjognf  ojsfdngvo osdij fôsijfdôsrigjôijsfog ijpij insdfunv usdf"
 
 template.render()
