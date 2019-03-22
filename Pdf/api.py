cd8866eefe14ec3982d4632ccd89be19dfebac7e
Louis-Saglio <louis.saglio@sfr.fr>
2019-03-22 17:47:43 +0100

    [change] Use class PdfTemplateVariable instead of dict [doc] Add type hints

--- Pdf/api.py
+++ Pdf/api.py
@@ -1,41 +1,50 @@
 import io
+from typing import Set, Dict, Any, Tuple
 
 import PyPDF2
 import reportlab.lib.pagesizes
 import reportlab.pdfgen.canvas
 
 
-class PdfException(Exception):
-    pass
+class PdfTemplateVariable:
+    def __init__(self, name: str, line_size: int):
+        self.name = name
+        self.line_size = line_size
 
 
-def build_filled_pdf_page(page_data, values):
+PageData = Dict[Tuple[int, int], PdfTemplateVariable]
+
+
+def build_filled_pdf_page(page_data: PageData, values: Dict[str, Any]) -> PyPDF2.PdfFileReader:
     packet = io.BytesIO()
     canvas = reportlab.pdfgen.canvas.Canvas(
         packet, reportlab.lib.pagesizes.letter, initialFontName="Times-Roman", initialFontSize=14
     )
     for position, emplacement_data in page_data.items():
-        canvas.drawString(*position, values[emplacement_data["name"]])
+        canvas.drawString(*position, str(values[emplacement_data.name])[:emplacement_data.line_size])
     canvas.save()
     packet.seek(0)
     return PyPDF2.PdfFileReader(packet)
 
 
 class PdfTemplate:
-    def __init__(self, base_file_path, emplacements):
+    def __init__(self, base_file_path: str, emplacements: Dict[int, PageData]):
         self.base_file_path = base_file_path
         self.emplacements = emplacements
 
-    def get_pages_to_fill_numbers(self, values):
+    def get_pages_to_fill_numbers(self, values: Dict[str, Any]) -> Set[int]:
+        """
+        Returns a set containing the numbers of the pages having to be filled by at least one value of values
+        """
         pages_to_fill = set()
         for page_num, page_data in self.emplacements.items():
             for emplacement_data in page_data.values():
-                if emplacement_data["name"] in values:
+                if emplacement_data.name in values:
                     pages_to_fill.add(page_num)
                     break
         return pages_to_fill
 
-    def render(self, **values):
+    def render(self, **values: Any) -> PyPDF2.PdfFileWriter:
         pages_to_fill = self.get_pages_to_fill_numbers(values)
 
         output = PyPDF2.PdfFileWriter()
